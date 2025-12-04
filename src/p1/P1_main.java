767e1848b67256913075e516621466548a7daa90
Felix Elsner <felsner@smail.uni-koeln.de>
2025-12-04 14:00:33 +0100

    neu

--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -1,4 +1,27 @@
 package p1;
 
 public class P1_main {
+    public static void main(String[] args) {
+        int z1 = 1;
+        int z2 = 3;
+        int n1 = 4;
+        int n2 = 6;
+
+        Bruch a = new Bruch(1, 2);
+        Bruch b = new Bruch(4, 2);
+
+        multFrac(z1, n1, z2, n2);
+    }
+
+    public static void multFrac(int z1, int n1, int z2, int n2) {
+        int zGen = z1 * z2;
+        int nGen = n1 * n2;
+
+        String bruch = zGen + ":" + nGen;
+        System.out.println(bruch);
+    }
+
+    public static void multFrac(Bruch a, Bruch b) {
+
+    }
 }
